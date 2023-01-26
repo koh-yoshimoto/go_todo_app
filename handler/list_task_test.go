29c8d9b5b37d4b5e4020e6577554492f295473d9
koh-yoshimoto <yoshimoto.koshiro@gmail.com>
2023-01-26 16:43:19 +0900

    add list_task test

---
+++ handler/list_task_test.go
@@ -0,0 +1,74 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/koh-yoshimoto/go_todo_app/entity"
+	"github.com/koh-yoshimoto/go_todo_app/store"
+	"github.com/koh-yoshimoto/go_todo_app/testutil"
+)
+
+func TestListTask(t *testing.T) {
+	type want struct {
+		status  int
+		rspFile string
+	}
+	tests := map[string]struct {
+		tasks []*entity.Task
+		want  want
+	}{
+		"ok": {
+			tasks: []*entity.Task{
+				{
+					ID:     1,
+					Title:  "test1",
+					Status: entity.TaskStatusTodo,
+				},
+				{
+					ID:     2,
+					Title:  "test2",
+					Status: entity.TaskStatusDone,
+				},
+			},
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/list_task/ok_rsp.json.golden",
+			},
+		},
+		"empty": {
+			tasks: []*entity.Task{},
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/list_task/empty_rsp.json.golden",
+			},
+		},
+	}
+
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+
+			tm := make(map[entity.TaskID]*entity.Task)
+			for _, task := range tt.tasks {
+				tm[entity.TaskID(task.ID)] = task
+			}
+			sut := ListTask{
+				Store: &store.TaskStore{
+					Tasks: tm,
+				},
+			}
+			sut.ServeHTTP(w, r)
+
+			resp := w.Result()
+			testutil.AssertResponse(t,
+				resp, tt.want.status, testutil.LoadFile(t, tt.want.rspFile),
+			)
+		})
+	}
+}
